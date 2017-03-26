0988c0a78161348bef0517f9f41c740d3693f179
ramboStudio <raccatama@gmail.com>
2017-03-26 22:55:58 +0700

    Add Hamburger icon

--- app/src/main/java/com/example/rambo/liveat500px/activity/MainActivity.java
+++ app/src/main/java/com/example/rambo/liveat500px/activity/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.rambo.liveat500px.activity;
 
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.rambo.liveat500px.R;
 import com.example.rambo.liveat500px.fragment.MainFragment;
@@ -32,9 +35,38 @@ public class MainActivity extends AppCompatActivity {
 
     private void initInstances() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                MainActivity.this,
+                drawerLayout,
+                R.string.open_drawer,
+                R.string.close_drawer
+        );
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
 
 }
